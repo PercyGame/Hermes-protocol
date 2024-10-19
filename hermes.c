a0300a7416c5f161ae7c1da4f75c3f9240081bb6
PercyGame <67379967+PercyGame@users.noreply.github.com>
2024-10-19 11:58:27 +0200

    starting the code

--- hermes.c
+++ hermes.c
@@ -1,2 +1,9 @@
 #include "hermes.h"
 
+RawPacket hermes_in(ClearPacket packet){
+    
+}
+
+ClearPacket hermes_out(RawPacket packet){
+
+}
--- hermes.h
+++ hermes.h
@@ -1,6 +1,19 @@
 #ifndef HERMES
 #define HERMES
 
+//structure that hold the clear packet, with his value and his key
+typedef struct ClearPacket{
+    char msg[1024];
+    char key[256];
+} ClearPacket;
 
+
+//structure that hold the crypted packet, with only the value
+typedef struct RawPacket{
+    char msg[1024];
+} RawPacket;
+
+RawPacket hermes_in(ClearPacket packet);//cryptage function
+ClearPacket hermes_out(RawPacket packet);//decryptage function
 
 #endif
